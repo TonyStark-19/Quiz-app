45919b66cc35b083f402ea60c73524acaa057e7f
Aditya chandel <adityachandel371@gmail.com>
2025-05-30 21:33:57 +0530

    Added dark for theme toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // use class-based dark mode
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // adjust as needed
   ],
   theme: {
     extend: {},
