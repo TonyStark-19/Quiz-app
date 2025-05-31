e19ababc124ec0c578bf25a1dc748d6754fc1545
Aditya chandel <adityachandel371@gmail.com>
2025-05-31 21:30:33 +0530

    Added UI improvements + content

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,25 +1,88 @@
 // import Navbar
 import { Navbar } from "./home";
 
+// category page
 export default function CategoryPage() {
     return (
-        <div className="containerh-screen">
+        <div className="container min-h-screen">
             <Navbar />
             <CategoryContent />
         </div>
     )
 }
 
+// array for box content
+const array = [
+    {
+        title: "C",
+        description: "Strengthen your understanding of the foundational programming language. Pointers, loops, arrays — master the building blocks.",
+        info: "10 Questions | Beginner friendly"
+    },
+    {
+        title: "C++",
+        description: "Test your knowledge of object-oriented programming, STL, and advanced logic-building techniques",
+        info: "12 Questions | Intermediate Level"
+    },
+    {
+        title: "OOPS Concept",
+        description: "Dive into core OOP concepts — encapsulation, inheritance, polymorphism, and abstraction",
+        info: "10 Questions | Theory + Conceptual"
+    },
+    {
+        title: "HTML",
+        description: "Explore the basics of web structure. Tags, forms, elements — everything you need to build strong foundations",
+        info: "10 Questions | Beginner Friendly"
+    },
+    {
+        title: "CSS",
+        description: "Test your styling skills with quizzes on selectors, layouts, animations, and responsive design",
+        info: "12 Questions | Beginner to Intermediate"
+    },
+    {
+        title: "JavaScript",
+        description: "Challenge your logic, syntax, and knowledge of JS concepts like arrays, functions, and DOM manipulation",
+        info: "15 Questions | Mixed Difficulty"
+    },
+    {
+        title: "React JS",
+        description: "Master components, hooks, props, and state management in the world’s most popular frontend library",
+        info: "12 Questions | Intermediate to Advanced"
+    },
+    {
+        title: "Bootstrap",
+        description: "Understand the core components, grid system, and classes that make responsive design fast and easy",
+        info: "10 Questions | Beginner Level"
+    },
+    {
+        title: "Tailwind CSS",
+        description: "Test your knowledge of utility-first CSS. Classes, responsiveness, and layouting made quick and clean",
+        info: "10 Questions | UI Focused"
+    },
+    {
+        title: "Git & GitHub",
+        description: "Version control your knowledge! Commands, branches, commits, and collaboration — test it all",
+        info: "10 Questions | CLI + GitHub Basics"
+    }
+]
+
 // category page content
 function CategoryContent() {
     return (
-        <div className="content flex justify-center items-center flex-col gap-6 pt-10 w-full h-full bg-orange-50 dark:bg-gray-950  text-stone-800 dark:text-white">\
-            <h1 className="text-5xl font-bold text-center">Choose Quiz Category</h1>
+        <div className="content flex-col gap-6 pt-28 w-full h-full
+         bg-orange-50 dark:bg-gray-950  text-stone-800 dark:text-white">
+            <h1 className="text-6xl font-bold text-center">Choose Quiz Category</h1>
 
-            <div className="box-wrap pt-5 flex justify-center flex-row gap-4">
-                <div className="flex justify-center items-center box w-48 border-2 pt-2 pb-2 border-gray-500 rounded-md">
-                    <h2 className="text-3xl font-bold">HTML</h2>
-                </div>
+            <div className="box-wrap pb-10 flex-wrap pt-14 flex justify-center flex-row gap-4">
+                {array.map((arr, idx) => (
+                    <div className="flex flex-col justify-center box w-96 border-2 p-4 rounded-md
+                   border-stone-600 dark:border-gray-500 text-stone-800 dark:text-white" key={idx}>
+                        <h2 className="text-2xl font-semibold">{arr.title}</h2>
+                        <h4 className="mt-2">{arr.description}</h4>
+                        <p className="mt-2">{arr.info}</p>
+                        <button className="rounded-md p-1 mt-3
+                         bg-stone-600 dark:bg-gray-700 text-orange-50 dark:text-white">Start Quiz</button>
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -37,11 +37,15 @@ export function Navbar() {
     };
 
     return (
-        <div className="navbar flex justify-between flex-row p-4 fixed z-3 w-full border-b-2 border-stone-800 dark:border-white bg-orange-50 dark:bg-gray-950">
-            <div className="head text-2xl font-semibold text-stone-800 dark:text-white">Quiz App</div>
+        <div className="navbar flex justify-between flex-row p-4 fixed z-3 w-full border-b-2 
+        border-stone-800 dark:border-white bg-orange-50 dark:bg-gray-950">
+            <Link to="/">
+                <div className="head text-2xl font-bold text-stone-800 dark:text-white">Quiz App</div>
+            </Link>
             <button
                 onClick={toggleTheme}
-                className="text-stone-800 dark:text-white border border-stone-800 dark:border-white px-3 py-1 rounded-md hover:bg-stone-200 dark:hover:bg-gray-700 transition"
+                className="px-3 py-1 rounded-md
+                text-stone-800 dark:text-white border border-stone-800 dark:border-white hover:bg-stone-200 dark:hover:bg-gray-700 transition"
             >
                 {isDark ? '🌞 Light' : '🌙 Dark'}
             </button>
@@ -52,14 +56,17 @@ export function Navbar() {
 // content
 function Content() {
     return (
-        <div className="content flex justify-center items-center flex-col gap-6 pt-10 w-full h-full bg-orange-50 dark:bg-gray-950  text-stone-800 dark:text-white">
+        <div className="content flex justify-center items-center flex-col gap-6 pt-10 w-full h-full
+         bg-orange-50 dark:bg-gray-950  text-stone-800 dark:text-white">
             <h1 className="text-7xl font-bold text-center">Quiz App!</h1>
             <h2 className="text-4xl text-center font-semibold">Test Your Knowledge. Learn Something New.</h2>
             <p className="text-center text-xl max-w-xl">
                 Welcome to the ultimate quiz experience powered by React and Tailwind CSS. Whether you're brushing up on skills or just having fun, this app helps you learn, compete, and grow.
             </p>
             <Link to="/pages/category">
-                <button className="pt-2 pb-2 pr-4 pl-4 text-lg rounded-md bg-stone-600 dark:bg-gray-700 text-orange-50 dark:text-white">Get Started</button>
+                <button className="pt-2 pb-2 pr-4 pl-4 text-lg rounded-md
+                 border-2 border-stone-600 dark:border-gray-700 hover:bg-stone-600 hover:text-orange-50 hover:dark:bg-gray-700
+                 text-stone-800 dark:text-white bg-transparent dark:bg-transparent">Get Started</button>
             </Link>
         </div>
     )
