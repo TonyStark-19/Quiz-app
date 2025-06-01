4f05d5b8156a930ce8525769eec5a7edcff6c33a
Aditya chandel <adityachandel371@gmail.com>
2025-06-01 21:23:25 +0530

    Routing for quiz page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/home';
-import CategoryPage from './pages/category'
+import CategoryPage from './pages/category';
+import QuizPage from './pages/quizpage';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pages/category" element={<CategoryPage />} />
+        <Route path="/quiz/:category" element={<QuizPage />} />
       </Routes>
     </Router>
   );
